86e8e49c8064a2abee5392712ed842a5369f90a0
Paulo Santos(Personal) <paulo.santos@compasso.com.br>
2025-06-09 17:26:16 -0300

    feat: Add TouchableOpacity to show loading

--- App2.js
+++ App2.js
@@ -1,29 +1,40 @@
 import React, { useState, useEffect} from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, ActivityIndicator, ActivityIndicatorComponent} from 'react-native';
 
 import api from './src/services/api';
 
 export default function App2() {
 
     const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await api.get('/posts');
-                setData(response.data);
-                alert('Data fetched successfully!');
-                setData(response.data);
-            } catch (error) {
-                alert('Error fetching data: ' + error.message);
-            }
-        }
         fetchData();
     }, []);
+
+    async function fetchData() {
+        setLoading(true);
+        function timeout(delay) {
+            return new Promise( res => setTimeout(res, delay) );
+        }
+        await timeout(2000);
+            
+        try {
+            const response = await api.get('/posts');
+            setData(response.data);
+            alert('Data fetched successfully!');
+        } catch (error) {
+            alert('Error fetching data: ' + error.message);
+        }
+        setLoading(false);
+    }
     
     return (
         <View style={styles.container}>
-            <TouchableOpacity style={styles.button} onPress={() => alert('Button Pressed!')}>
+            <TouchableOpacity style={styles.button} onPress={() => {
+                // alert('Button Pressed!')
+                fetchData();
+            }}>
                 <Text style={styles.buttonText}>
                     Access API
                 </Text>
@@ -31,6 +42,7 @@ export default function App2() {
             <View style={styles.row}>
                 <Text>Title: {data.title}</Text>
             </View>
+            <ActivityIndicator size="large" animating={loading} />
         </View>
     );
     
